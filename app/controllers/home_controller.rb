dd688c9d0494b3811a133db99ce3434fa0a2c4aa
teletubo <teletubo@gmail.com>
2014-08-29 21:37:57 -0300

    Adding timeout for ssl calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   
   def index 
-    @statuses = SSLAccess::ssl_get("#{BTCJAM_APP_URL}/employment_statuses.json" )
-  
-    @nationalid_types = SSLAccess::ssl_get("#{BTCJAM_APP_URL}/nationalid_types.json" )
+    if signed_in?
+      
+      @statuses = SSLAccess::ssl_get("#{BTCJAM_APP_URL}/employment_statuses.json" )
+      @nationalid_types = SSLAccess::ssl_get("#{BTCJAM_APP_URL}/nationalid_types.json" )
+      
+    end
   end
 end
--- lib/ssl_access.rb
+++ lib/ssl_access.rb
@@ -3,29 +3,40 @@ module SSLAccess
 
   def self.ssl_get(url, params = {})
 
+  begin
     uri = URI(url)
     req = Net::HTTP::Get.new(uri.request_uri)
+    
     req.set_form_data(params)
 
     res = Net::HTTP.start(uri.host, uri.port, :ssl_version  => "SSLv3", :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+     https.read_timeout = 3
       https.request(req)
     end
 
     JSON.load(res.body)
+   rescue
+     nil
+   end
   end
 
   def self.ssl_post(url, params)
-
+    
+  begin
     uri = URI(url)
-    req = Net::HTTP::Post.new(uri.path)
+    req = Net::HTTP::Post.new(uri.path) 
     req.set_form_data(params)
 
 
     res = Net::HTTP.start(uri.host, uri.port, :ssl_version  => "SSLv3", :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+       https.read_timeout = 3
       https.request(req)
     end
 
     JSON.load(res.body)
+   rescue
+     nil
+   end
   end
 
 end
