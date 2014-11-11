1a24aff179785685721bfef38dd672a7c0540dda
teletubo <teletubo@gmail.com>
2014-11-10 16:40:47 -0800

    adding check_forms

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -37,7 +37,7 @@ class ApiController < ApplicationController
     post_hash = {  'identity_check[img]' => Faraday::UploadIO.new(params[:picture], 'image/jpeg'),
        'identity_check[name]'=>params[:name],'identity_check[dob]'=>params[:dob],'identity_check[gender]'=>params[:gender],'identity_check[nationalid]'=>params[:nationalid],'identity_check[nationalid_type_id]' => params[:nationalid_type_id]}
     @jret = client_token_multipart.request(:post, "#{BTCJAM_APP_URL}/api/v1/#{params[:api]}", { body: post_hash } ).parsed
-    
+   
     render :json => @jret
     
   end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
+ before_action :ensure_proper_subdomain, :only => "new"
 
+  def ensure_proper_subdomain
+    debugger
+    #puts request
+    #puts " pompomp "
+   # if request.host_with_port != 'www.dotatrack.net'
+   #   redirect_to params.merge({host: 'www.dotatrack.net'})
+   # end
+  end
 end
